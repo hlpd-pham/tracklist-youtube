f9d1329bf2b4d44f7d119d067c0073cd3d3dda6a
Hoa Pham <hoa.phamlethai@gmail.com>
2024-04-30 08:57:30 -0700

    initial commit

---
+++ quickstart.go
@@ -0,0 +1,212 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"os/user"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/option"
+	"google.golang.org/api/youtube/v3"
+)
+
+const missingClientSecretsMessage = `
+Please configure OAuth 2.0
+`
+
+// getClient uses a Context and Config to retrieve a Token
+// then generate a Client. It returns the generated Client.
+func getClient(ctx context.Context, config *oauth2.Config) *http.Client {
+	cacheFile, err := tokenCacheFile()
+	if err != nil {
+		log.Fatalf("Unable to get path to cached credential file. %v", err)
+	}
+	tok, err := tokenFromFile(cacheFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(cacheFile, tok)
+	}
+	return config.Client(ctx, tok)
+}
+
+// getTokenFromWeb uses Config to request a Token.
+// It returns the retrieved Token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var code string
+	if _, err := fmt.Scan(&code); err != nil {
+		log.Fatalf("Unable to read authorization code %v", err)
+	}
+
+	tok, err := config.Exchange(context.Background(), code)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web %v", err)
+	}
+	return tok
+}
+
+// tokenCacheFile generates credential file path/filename.
+// It returns the generated credential path/filename.
+func tokenCacheFile() (string, error) {
+	usr, err := user.Current()
+	if err != nil {
+		return "", err
+	}
+	tokenCacheDir := filepath.Join(usr.HomeDir, ".credentials")
+	os.MkdirAll(tokenCacheDir, 0o700)
+	return filepath.Join(tokenCacheDir,
+		url.QueryEscape("tracklist-youtube.json")), err
+}
+
+// tokenFromFile retrieves a Token from a given file path.
+// It returns the retrieved Token and any read error encountered.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	t := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(t)
+	defer f.Close()
+	return t, err
+}
+
+// saveToken uses a file path to create a file and store the
+// token in it.
+func saveToken(file string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", file)
+	f, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0o600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
+
+func handleError(err error, message string) {
+	if message == "" {
+		message = "Error making API call"
+	}
+	if err != nil {
+		log.Fatalf(message+": %v", err.Error())
+	}
+}
+
+func channelsListByHandle(service *youtube.Service, part string, forUsername string) {
+	channels := []string{part}
+	call := service.Channels.List(channels)
+	call = call.ForHandle(forUsername)
+	response, err := call.Do()
+	handleError(err, "")
+	fmt.Println(fmt.Sprintf("This channel's ID is %s. Its title is '%s', "+
+		"and it has %d views.",
+		response.Items[0].Id,
+		response.Items[0].Snippet.Title,
+		response.Items[0].Statistics.ViewCount))
+}
+
+func getVideoInfo(service *youtube.Service, parts []string, videoId string) {
+	call := service.Videos.List(parts).Id(videoId)
+	response, err := call.Do()
+	handleError(err, "")
+	fmt.Printf("Video Id: %s, title: %s\n", videoId, response.Items[0].Snippet.Title)
+}
+
+func replaceHtmlEntities(input string) string {
+	// Define a regular expression pattern to match HTML entities
+	pattern := `(&amp;)|(&#39;)`
+
+	// Compile the regular expression pattern
+	re := regexp.MustCompile(pattern)
+
+	// Replace HTML entities with their decoded counterparts
+	return re.ReplaceAllStringFunc(input, func(match string) string {
+		return html.UnescapeString(match)
+	})
+}
+
+func listCommentsByVideoId(service *youtube.Service,
+	parts []string,
+	videoId string,
+) {
+	call := service.CommentThreads.List(parts).VideoId(videoId)
+	response, err := call.Do()
+	handleError(err, "")
+
+	trackListComments := []*youtube.Comment{}
+
+	for response.NextPageToken != "" {
+		for _, commentThread := range response.Items {
+			topComment := commentThread.
+				Snippet.
+				TopLevelComment
+			if strings.Contains(topComment.Snippet.TextDisplay, "Tracklist") ||
+				strings.Contains(topComment.Snippet.TextDisplay, "tracklist") {
+				trackListComments = append(trackListComments, topComment)
+			}
+		}
+		call = service.CommentThreads.
+			List(parts).
+			VideoId(videoId).
+			PageToken(response.NextPageToken)
+		response, err = call.Do()
+		handleError(err, "")
+	}
+
+	bestComment := youtube.Comment{Snippet: &youtube.CommentSnippet{LikeCount: 0}}
+	if len(trackListComments) > 0 {
+		fmt.Printf("Found %d comments\n", len(trackListComments))
+		for _, comment := range trackListComments {
+			curLikeCount := comment.Snippet.LikeCount
+			if curLikeCount > bestComment.Snippet.LikeCount {
+				bestComment = *comment
+			}
+		}
+		pattern := `<a.*>.*<\/a>\s`
+		re := regexp.MustCompile(pattern)
+		removeBreakTags := strings.ReplaceAll(
+			bestComment.Snippet.TextDisplay, "<br>", "\n")
+		result := re.ReplaceAllString(removeBreakTags, "")
+
+		fmt.Println(replaceHtmlEntities(result))
+	} else {
+		fmt.Println("no tracklist comment found")
+	}
+}
+
+func main() {
+	ctx := context.Background()
+
+	b, err := os.ReadFile("youtube_client_secret.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	// If modifying these scopes, delete your previously saved credentials
+	// at ~/.credentials/tracklist-youtube.json
+	config, err := google.ConfigFromJSON(b, youtube.YoutubeForceSslScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := getClient(ctx, config)
+	service, err := youtube.NewService(ctx, option.WithHTTPClient(client))
+
+	handleError(err, "Error creating YouTube client")
+
+	videoId := "6V2kMynnQ7M"
+	getVideoInfo(service, []string{"snippet", "id"}, videoId)
+	listCommentsByVideoId(service, []string{"snippet", "id", "replies"}, videoId)
+}
